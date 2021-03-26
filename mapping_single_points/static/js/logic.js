f7329c4f505b5e6de346a947304ca33f81aa4819
bradydwilton <bradydwilton@gmail.com>
2021-03-26 12:10:24 -0500

    13.4.1 commit - mapping_single_points branch

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -7,10 +7,29 @@ console.log('working');
 
 let map = L.map('mapid', {
     center: [
-        40.7, -94.5
+        34.0522, -118.2437
     ],
-    zoom: 4
+    zoom: 14
 });
+
+// Add a marker
+// let marker = L.marker([34.0522, -118.2437]).addTo(map)
+
+// Add a circle with a fixed radius
+// let marker = L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: 'black',
+//     fillColor: 'yellow',
+//     fillOpacity: '0.25'
+// }).addTo(map);
+
+// Add a circle with a radius with a fixed pixel length
+let marker = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -19,7 +38,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     maxZoom: 18,
     zoomOffset: -1,
     // additional map styles (for 'id' below) found here: https://docs.mapbox.com/api/maps/styles/
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
