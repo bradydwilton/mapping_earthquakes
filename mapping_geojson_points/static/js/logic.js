a3a4c8ae410da4a5e8a286cde193dc62048c4216
bradydwilton <bradydwilton@gmail.com>
2021-03-29 18:33:19 -0500

    13.5.3 commit - adding majorAirports.json

--- mapping_geojson_points/static/js/logic.js
+++ mapping_geojson_points/static/js/logic.js
@@ -7,9 +7,9 @@ console.log('working');
 
 let map = L.map('mapid', {
     center: [
-        36.1988, -95.8839
+        37.6, -122.4
     ],
-    zoom: 5
+    zoom: 10
 });
 
 // Add a marker
@@ -44,20 +44,70 @@ let map = L.map('mapid', {
 //     }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>").addTo(map);
 // });
 
-let line = [
-    [33.9416, -118.4085],
-    [30.1975, -97.6664],
-    [36.1988, -95.8839],
-    [43.6777, -79.6248],
-    [40.6416, -73.7781]
-];
+// Add GeoJSON data.
+let sanFranAirport =
+{
+    "type": "FeatureCollection", "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": "3469",
+            "name": "San Francisco International Airport",
+            "city": "San Francisco",
+            "country": "United States",
+            "faa": "SFO",
+            "icao": "KSFO",
+            "alt": "13",
+            "tz-offset": "-8",
+            "dst": "A",
+            "tz": "America/Los_Angeles"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-122.375, 37.61899948120117]
+        }
+    }
+    ]
+};
 
-L.polyline(line, {
-    color: 'blue',
-    weight: '2',
-    dashArray: '10',
-    opacity: '0.5',
+// Add geoJSON data as a point
+// L.geoJSON(sanFranAirport).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>").addTo(map);
+
+// Add geoJSON data as a marker with a popup using the pointToLayer function
+// L.geoJSON(sanFranAirport, {
+//     pointToLayer: (feature, latlng) => {
+//         console.log(feature);
+//         return L.marker(latlng)
+//             .bindPopup("<h2>" + feature.properties.name + "</h2>")
+//     }
+// }).addTo(map);
+
+// Add geoJSON data as a marker with a popup using the pointToLayer function
+L.geoJSON(sanFranAirport, {
+    onEachFeature: (feature, layer) => {
+        console.log(layer);
+        const underscore = '_';
+        const popupTemplate = `<b>Airport Code: </b>${feature.properties.faa}
+            </br><b>${underscore.repeat(feature.properties.name.length + 5)}
+            </br>
+            </br>Airport Name: </b>${feature.properties.name}`;
+        layer.bindPopup(popupTemplate);
+    }
 }).addTo(map);
+
+// let line = [
+//     [33.9416, -118.4085],
+//     [30.1975, -97.6664],
+//     [36.1988, -95.8839],
+//     [43.6777, -79.6248],
+//     [40.6416, -73.7781]
+// ];
+
+// L.polyline(line, {
+//     color: 'blue',
+//     weight: '2',
+//     dashArray: '10',
+//     opacity: '0.5',
+// }).addTo(map);
 
 
 
@@ -68,7 +118,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     maxZoom: 18,
     zoomOffset: -1,
     // additional map styles (for 'id' below) found here: https://docs.mapbox.com/api/maps/styles/
-    id: 'mapbox/light-v10',
+    id: 'mapbox/outdoors-v11',
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
